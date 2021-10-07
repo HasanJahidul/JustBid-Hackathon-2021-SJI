8e9779c8381e3904c4f67a768c6c929e1ec8bbb5
HasanJahidul <joyaiub1@gmail.com>
2021-10-07 11:14:38 +0600

    layout and css added

--- JustBid/resources/views/auth/login.blade.php
+++ JustBid/resources/views/auth/login.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app', ['class' => 'login-page', 'page' => __('Login Page'), 'contentClass' => 'login-page'])
+
+
 <div class="col-lg-4 col-md-6 ml-auto mr-auto">
         <!-- <form class="form" method="post" action="{{ route('login') }}"> -->
         <form class="form" method="post" >
@@ -6,10 +9,10 @@
             <div class="card card-login card-white">
                 
                     <h1 align="center" class="card-title">{{ __('Log in') }}</h1>
-                        <input type="email" name="email" ">
+                           <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}">
                     </div>
                     <div >
-                        <input type="password"  name="password" ">
+                        <input type="password" placeholder="{{ __('Password') }}" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}">
                     </div>
                 </div>
                 <div class="card-footer">
---
+++ JustBid/resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'JustBid') }}</title>
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('black') }}/img/apple-icon.png">
+        <link rel="icon" type="image/png" href="{{ asset('black') }}/img/favicon.png">
+         <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+        <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+        <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <!-- Icons -->
+       
+        <!-- CSS -->
+       
+        
+    </head>
+    @csrf
+    <body class="{{ $class ?? '' }}">
+    
+   
+        
+        
+       
+
+    </body>
+</html>
